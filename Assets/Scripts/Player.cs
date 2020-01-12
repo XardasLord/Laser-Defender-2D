a831cc9ba79ec92d7d1d44bd34f6fc9da770adbf
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-01-12 09:18:24 +0100

    Player can shoot now with lasers

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,45 +1,64 @@
-﻿using System;
+﻿using System.Net;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace Assets.Scripts
 {
-    [SerializeField] private float moveSpeed = 8f;
-    [SerializeField] private float padding = 1f;
-
-    private float _xMin;
-    private float _xMax;
-    private float _yMin;
-    private float _yMax;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Player : MonoBehaviour
     {
-        SetUpMoveBoundaries();
-    }
+        [SerializeField] private float moveSpeed = 8f;
+        [SerializeField] private float padding = 1f;
+        [SerializeField] private GameObject laserPrefab;
+        [SerializeField] private float projectileSpeed = 10f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Move();
-    }
+            private float _xMin;
+        private float _xMax;
+        private float _yMin;
+        private float _yMax;
 
-    private void SetUpMoveBoundaries()
-    {
-        var gameCamera = Camera.main;
-        _xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
-        _xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
-        _yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
-        _yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
-    }
+        // Start is called before the first frame update
+        private void Start()
+        {
+            SetUpMoveBoundaries();
+        }
 
-    private void Move()
-    {
-        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        // Update is called once per frame
+        private void Update()
+        {
+            Move();
+            Fire();
+        }
 
-        var newXPos = Mathf.Clamp(transform.position.x + deltaX, _xMin, _xMax);
-        var newYPos = Mathf.Clamp(transform.position.y + deltaY, _yMin, _yMax);
+        private void Move()
+        {
+            var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+            var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
-        transform.position = new Vector2(newXPos, newYPos);
+            var newXPos = Mathf.Clamp(transform.position.x + deltaX, _xMin, _xMax);
+            var newYPos = Mathf.Clamp(transform.position.y + deltaY, _yMin, _yMax);
+
+            transform.position = new Vector2(newXPos, newYPos);
+        }
+
+        private void Fire()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                var laser = Instantiate(
+                    laserPrefab, 
+                    transform.position, 
+                    Quaternion.identity);
+
+                laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            }
+        }
+
+        private void SetUpMoveBoundaries()
+        {
+            var gameCamera = Camera.main;
+            _xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+            _xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+            _yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+            _yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+        }
     }
 }
