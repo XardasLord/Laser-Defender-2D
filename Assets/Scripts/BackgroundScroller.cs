59bd5a4cd9cc7594a974c1ada6cab6c8b62e146c
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-01-14 19:02:15 +0100

    Background scrolling effect

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class BackgroundScroller : MonoBehaviour
+    {
+        [SerializeField] private float backgroundScrollSpeed = 0.02f;
+
+        private Material _myMaterial;
+        private Vector2 _offset;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _myMaterial = GetComponent<Renderer>().material;
+            _offset = new Vector2(0f, backgroundScrollSpeed);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _myMaterial.mainTextureOffset += _offset * Time.deltaTime;
+        }
+    }
+}
