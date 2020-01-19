a0d56ab787a80e043b622e86430472e9a6290e67
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-01-19 21:10:02 +0100

    Start Menu scene

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class Level : MonoBehaviour
+    {
+        public void LoadGameOver()
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+
+        public void LoadGame()
+        {
+            SceneManager.LoadScene("Game");
+        }
+
+        public void LoadStartMenu()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
